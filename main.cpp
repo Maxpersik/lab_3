2487937b0aab83b3c18e8e03b631e8254d3188b3
Maxim <maxpersik@gmail.com>
2024-11-28 18:49:35 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  lab_3
+//
+//  Created by Maxim Gvozdev on 28.11.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
